baf492d31348dacb0f0785b9bc54f52218aaf627
rdenden <dddrrr814@gmail.com>
2021-05-04 05:30:03 +0900

    3-4

--- 3-1.java
+++ 3-1.java
@@ -1,9 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
         boolean tenki = true;
-        if (tenki == true) {
+        if (tenki == true) {　//かっこ省略不可（二行だから）
             System.out.println("洗濯します");
-        } else {
+            System.out.println("散歩に行きます")
+        } else {　// かっこ省略可
             System.out.println("DVDを見ます");
         }
 	}
---
+++ 3-4.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+	    System.out.println("あなたの運勢を占います");
+	    int fortune = new java.util.Random().nextInt(4) + 1; 
+	    switch (fortune) {
+	        case 1:
+	            System.out.println("大吉");
+	            break;
+            case 2:
+                System.out.println("中吉");
+                break;
+            case 3:
+                System.out.println("吉");
+                break;
+            default:
+                System.out.println("凶");
+	    }
+	}
+}
