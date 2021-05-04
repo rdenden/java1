5dc12ffd32afe8d6e59f1d5bbb580f5d7dbebffd
rdenden <dddrrr814@gmail.com>
2021-05-04 23:56:08 +0900

    Create p3-3.java

---
+++ p3-3.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+	    int isHungry = 1;
+	    String food = "餃子";
+	    System.out.println("こんにちは");
+	    if (isHungry == 0) {
+	        System.out.println("お腹がいっぱいです");
+	    } else {
+	        System.out.println("はらぺこです");
+	        System.out.println(food + "をいただきます");
+	        System.out.println("ごちそうさまでした");
+	    }
+	    
+	    
+	}
+}
