60df583f4818c23419c388ce40a11bbbc7e7ee75
rdenden <dddrrr814@gmail.com>
2021-05-06 05:23:45 +0900

    Create p5-1.java

---
+++ p5-1.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void introduceOneself() {
+        String name = "タロー";
+        int age = 99;
+        double height = 180.5;
+        char juunishi = '酉';
+        System.out.println("私の名前は" + name + "です。");
+        System.out.println("身長は" + height + "cmです。");
+        System.out.println(juunishi + "年生まれの" + age + "歳です。");
+	}
+	public static void main(String[] args) {
+	    introduceOneself();
+	}
+}
