88b3fc0342a16808d48a626320b32d3916f76408
rdenden <dddrrr814@gmail.com>
2021-05-08 20:36:08 +0900

    Update Cleric.java

--- p8_1/Cleric.java
+++ p8_1/Cleric.java
@@ -4,4 +4,10 @@ public class Cleric {
     final int HPMax = 50;
     int mp = 10;
     final int MPMax = 10;
+    public void selfAid() {
+        System.out.println(this.name + "はセルフエイドを唱えた！");
+        this.mp -= 5;
+        this.hp = HPMax;
+        System.out.println("HPが最大まで回復した！")
+    }
 }
