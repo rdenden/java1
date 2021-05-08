bf5c9cfe164e78c616d59a3419a25221e8bde540
rdenden <dddrrr814@gmail.com>
2021-05-08 20:23:54 +0900

    8_15,p8_1

--- 8_15/Matango.java
+++ 8_15/Matango.java
@@ -1,8 +1,8 @@
 public class Matango {
     int hp;
-    final int LEVEL = 10
+    final int LEVEL = 10;
     char suffix;
-    public void run {
-        System.out.println("お化けキノコ" + this.suffix + "逃げ出した！")
+    public void run() {
+        System.out.println("お化けキノコ" + this.suffix + "逃げ出した！");
     }
 }
