7b2b72211cef5262c72c4a78a03c18afe56a07df
rdenden <dddrrr814@gmail.com>
2021-05-08 21:35:20 +0900

    Update Cleric.java

--- p8_1/Cleric.java
+++ p8_1/Cleric.java
@@ -1,13 +1,21 @@
 public class Cleric {
     String name;
     int hp = 50;
-    final int HPMax = 50;
+    final int HPMAX = 50;
     int mp = 10;
-    final int MPMax = 10;
+    final int MPMAX = 10;
     public void selfAid() {
         System.out.println(this.name + "はセルフエイドを唱えた！");
         this.mp -= 5;
         this.hp = HPMax;
         System.out.println("HPが最大まで回復した！")
+    }
+    public int pray(sec) {
+        System.out.println(this.name + "は" + sec + "秒間天に祈った！")
+        int recover = new java.util.Random().nextInt(3) + sec;
+        int recoveryPoint = Math.min(this.MPMAX - this.mp,recover);
+        this.mp += recoveryPoint;
+        System.out.println("MPが" + recoveryPoint + "回復した！")
+        return recoveryPoint;
     }
 }
