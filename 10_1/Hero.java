4154fa577d0f463970946c6c2a0cd175d897632a
rdenden <dddrrr814@gmail.com>
2021-05-11 21:06:24 +0900

    10_1

--- 10_1/Hero.java
+++ 10_1/Hero.java
@@ -3,12 +3,12 @@ public class Hero {
     int hp = 100;
     
     public void attack(Matango m) {
-        System.out println(this.name + "の攻撃！");
+        System.out.println(this.name + "の攻撃！");
         m.hp -= 5;
         System.out.println("5ポイントのダメージを与えた！");
     }
-   
+    // オーバーライド  Heroクラスのrunを上書き 
     public void run() {
-        System.out.println(this.name + "は逃げ出した！")
+        System.out.println(this.name + "は逃げ出した！");
     }
 }
--- 10_1/SuperHero.java
+++ 10_1/SuperHero.java
@@ -1,14 +1,35 @@
-public class Hero {
+public class SuperHero extends Hero {
     String name = "ミナト";
     int hp = 100;
+    boolean flying;
+    
+    public void fly() {
+        this.flying = true;
+        System.out.println("飛び上がった！");
+    }
+    
+    public void land() {
+        this.flying = false;
+        System.out.println("着地した！");
+    } 
+    public void run() {
+        System.out.println(this.name + "撤退した");
+    }
     
     public void attack(Matango m) {
-        System.out.println(this.name + "の攻撃！");
-        m.hp -= 5;
-        System.out.println("5ポイントのダメージを与えた！");
-    }
-    // オーバーライド  Heroクラスのrunを上書き 
-    public void run() {
-        System.out.println(this.name + "は逃げ出した！");
+        // System.out.println(this.name + "の攻撃！");
+        // m.hp -= 5;
+        // System.out.println("5ポイントのダメージを与えた！");
+        // if (this.flying) {
+        //     System.out.println(this.name + "の攻撃！");
+        //     m.hp -= 5;
+        //     System.out.println("5ポイントのダメージを与えた！");
+        // }
+        // 親クラスからattack()を呼び出し上と同じ挙動を実現
+        super.attack(m);
+        if (this.flying) {
+            super.attack(m);
+        }
+        
     }
 }
