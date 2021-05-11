6bd565479615582c753d12e9ffc1524a7e2e96dc
rdenden <dddrrr814@gmail.com>
2021-05-11 18:29:59 +0900

    10_1

--- 10-1/SuperHero.java
+++ 10-1/SuperHero.java
@@ -1,15 +1,14 @@
-public class SuperHero extends Hero {
+public class Hero {
     String name = "ミナト";
     int hp = 100;
-    boolean flying;
     
-    public void fly() {
-        this.flying = true;
-        System.out.println("飛び上がった！");
+    public void attack(Matango m) {
+        System.out.println(this.name + "の攻撃！");
+        m.hp -= 5;
+        System.out.println("5ポイントのダメージを与えた！");
     }
-    
-    public void land() {
-        this.flying = false;
-        System.out.println("着地した！");
+    // オーバーライド  Heroクラスのrunを上書き 
+    public void run() {
+        System.out.println(this.name + "は逃げ出した！");
     }
 }
