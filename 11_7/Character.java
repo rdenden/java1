0ab26a34a45a80bb855dc46a1c39d8398bb8d14a
rdenden <dddrrr814@gmail.com>
2021-05-12 19:12:32 +0900

    11_7抽象メソッド

--- 11_7/Character.java
+++ 11_7/Character.java
@@ -5,6 +5,6 @@ public class Character {
     public void run() {
         System.out.println(this.name + "は逃げ出した");
     }
-    
+    // 抽象メソッド　現時点では確定できないメソッド
     public abstract void attack(Matango m);
 }
