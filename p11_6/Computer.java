14121a34096b02487b812ab66bcfae156f5e47da
rdenden <dddrrr814@gmail.com>
2021-05-13 03:41:08 +0900

    p11_6

---
+++ p11_6/Computer.java
@@ -0,0 +1,19 @@
+public class Computer {
+    String name;
+    int price;
+    String color;
+    String makerName;
+    
+    public Computer (String name,int price,String color,String makerName) {
+        this.name;
+        this.price = price;
+        this.color = color;
+        this.makeName = makerName;
+    }
+    
+    public String getName() {return this.name;}
+    public int getPrice() {return this.price;}
+    public String getColor() {return this.color;}
+    public String getMakerName() {return this.maketName;}
+    
+}
