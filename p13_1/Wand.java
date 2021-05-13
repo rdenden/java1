3155eb3c1d34836eb3bc6ff7b93607f2adac6b63
rdenden <dddrrr814@gmail.com>
2021-05-14 06:46:24 +0900

    Update Wand.java

--- p13_1/Wand.java
+++ p13_1/Wand.java
@@ -1,4 +1,19 @@
 public class Wand {
-    String name;
-    double power;
+    private String name;
+    private double power;
+    
+     public String getName {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+
+    public double getPower {
+        return this.power;
+    }
+    public void setPower(double power) {
+        this.power = power;
+    }
 }
