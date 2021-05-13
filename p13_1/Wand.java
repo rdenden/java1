770bed16b9c95c95c0ab3f90db2eadfe65fd9dda
rdenden <dddrrr814@gmail.com>
2021-05-14 07:27:13 +0900

    引数妥当性検証

--- p13_1/Wand.java
+++ p13_1/Wand.java
@@ -6,6 +6,10 @@ public class Wand {
         return this.name;
     }
     public void setName(String name) {
+        if (name == null || name.length() < 3) {
+            throw new IllegalArgumentException
+            ("杖の名前は３文字以上にしてください");
+        }
         this.name = name;
     }
     
@@ -14,6 +18,11 @@ public class Wand {
         return this.power;
     }
     public void setPower(double power) {
+        if (power < 0.5 || power > 100.0) {
+            throw new IllegalArgumentException
+            ("杖の増幅率は0.5〜100の間で設定してください");
+        }
         this.power = power;
     }
 }
+
--- p13_1/Wizard.java
+++ p13_1/Wizard.java
@@ -8,13 +8,21 @@ public class Wizard {
         return this.hp;
     }
     public void setHp(int hp) {
-        this.hp = hp;
+        if (hp < 0) {
+            this.hp = 0;
+        } else {
+            this.hp = hp;
+        }
     }
     
     public int getMp() {
         return this.mp;
     }
     public void setMp(int mp) {
+        if (mp < 0) {
+            throw new IllegalArgumentException
+            ("MPは0以上にしてください");
+        }
         this.mp = mp;
     }
     
@@ -22,6 +30,10 @@ public class Wizard {
         return this.name;
     }
     public void setName(String name) {
+        if (name == null || name.length() < 3) {
+            throw new IllegalArgumentException
+            ("魔法使いの名前は３文字以上にしてください");
+        }
         this.name = name;
     }
     
@@ -29,6 +41,10 @@ public class Wizard {
         return this.wand;
     }
     public void setWand(Wand wand) {
+        if (wand == null) {
+            throw new IllegalArgumentException
+            ("魔法使いは必ず杖を装備してください");
+        }
         this.wand = wand;
     }
     
