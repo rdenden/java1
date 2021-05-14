df962318e1321a96da498bcae27b7c0dbdb34d28
rdenden <dddrrr814@gmail.com>
2021-05-14 17:23:13 +0900

    14_4

---
+++ 14_4/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+// 		toString オブジェクトの中身の情報を文字列にして出力
+		System.out.println(h.toString());
+	}
+}
