29d0192cff910052892721e964345030c85a2bb9
rdenden <dddrrr814@gmail.com>
2021-05-15 07:50:15 +0900

    Update Main.java

--- 15_1/Main.java
+++ 15_1/Main.java
@@ -10,5 +10,8 @@ public class Main {
 	        System.out.println("s2とs2はケースを区別しなければ等しい");
 	    }
 	    System.out.println("s1の長さは" + s1.length() + "です");
+	    if (s1.isEmpty()) {
+	        System.out.println("s1は空文字です");
+	    }
 	}
 }
