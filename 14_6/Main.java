5320862355de1203b673e86608c9c9bf5149254c
rdenden <dddrrr814@gmail.com>
2021-05-14 19:19:48 +0900

    Update Main.java

--- 14_6/Main.java
+++ 14_6/Main.java
@@ -7,9 +7,30 @@ public class Main {
         h2.hp = 100;
         // 等価判定
         if (h1.equals(h2) == true) {
-            System.out.println("同じ内容です");
+            System.out.println("等価判定　同じ内容です");
         } else {
-            System.out.println("違う内容です");
+            System.out.println("等価判定　違う内容です");
         }
+        
+        if (h1 == h2) {
+            System.out.println("等値判定　同じ内容です");
+        } else {
+            System.out.println("等値判定　違う内容です");
+        }
+        
+        Hero h3 = new Hero();
+        h3.name = "ミナト";
+        Hero h4 = h3;
+        if (h3.equals(h4) == true) {
+            System.out.println("等価判定　同じ内容です");
+        } else {
+            System.out.println("等価判定　違う内容です");
+        }
+        if (h3 == h4) {
+            System.out.println("等値判定　同じ内容です");
+        } else {
+            System.out.println("等値判定　違う内容です");
+        }
+        
     }
 }
