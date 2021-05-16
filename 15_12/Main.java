fc71ecf6a5f4c7c254d56a0e129963d7e81dbb48
rdenden <dddrrr814@gmail.com>
2021-05-16 18:23:15 +0900

    15_12

---
+++ 15_12/Main.java
@@ -0,0 +1,17 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+	public static void main(String[] args) {
+		Calendar c = Calendar.getInstance();
+		c.set(2019,8,22,1,23,45);
+// 		月を9(10月)に修正
+		c.set(Calendar.MONTH,9);
+		Date d = c.getTime();
+		System.out.println(d);
+		Date now = new Date();
+		c.setTime(now);
+		int y = c.get(Calendar.YEAR);
+		System.out.println("今年は" + y + "年です");
+	}
+}
