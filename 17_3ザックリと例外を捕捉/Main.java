93333515c0f6e799c9429d132e30c378fbb50a83
rdenden <dddrrr814@gmail.com>
2021-05-25 03:10:21 +0900

    Update Main.java

--- 17_3ザックリと例外を捕捉/Main.java
+++ 17_3ザックリと例外を捕捉/Main.java
@@ -2,22 +2,22 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) {
-	    FileWriter fw = null; 
+    FileWriter fw = null; 
 		try {
-		    fw = new FileWriter("data.txt");
-		    fw.write("hello!");
-		    
+		  fw = new FileWriter("data.txt");
+		  fw.write("hello!");		    
 		  //  Exceptionの子孫をどれでもキャッチ
 		} catch (Exception e) {
-		    System.out.println("何らかの例外が発生しました");
+      System.out.println("何らかの例外が発生しました");
         // finallyを使えば、例外の有無に関わらず必ず実行させることができる
 		} finally {
-		    try {
-		        fw.close();
-		    } catch (IOException e) {
-		        ;
-		    }
-            
-        }
+      if (fw != null) {
+          try {
+            fw.close();
+		      } catch (IOException e) {
+		      ;
+		      }
+      }
+    }
 	}
 }
