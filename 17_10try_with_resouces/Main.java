2dea3d65079100aab69f8916c4794d1bd7c99914
rdenden <dddrrr814@gmail.com>
2021-05-25 19:55:50 +0900

    17_10

---
+++ 17_10try_with_resouces/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		try (FileWriter fw = new FileWriter("data.txt");) {
+		    fw.write("hello!");
+		  //  Exceptionの子孫をどれでもキャッチ
+		} catch (Exception e) {
+		    System.out.println("何らかの例外が発生しました");
+		}
+	}
+}
