ea50d4d68b2448021c2f325568e3f6264abe1e47
rdenden <dddrrr814@gmail.com>
2021-05-25 23:12:23 +0900

    Update Main.java

--- p17_9error/Main.java
+++ p17_9error/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 	public static void main(String[] args) {
-	    String s = null;
+	    try {
+	        String s = null;
+	        System.out.println(s.length());
+	    } catch (NullPointerException e) {
+	        System.out.println("NullPointerException例外をcatchしました");
+	        System.out.println("ーースタックトレース（ここから）ーー");
+	        e.printStackTrace();
+	        System.out.println("ーースタックトレース（ここまで）ーー");
+	    }
 	    
-		System.out.println(s.length());
+		
 	}
-}
+} 
